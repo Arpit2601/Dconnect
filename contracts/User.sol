56ec81784cc90a3e1307e4b4a3961b30d85c853d
Arpit Gupta <arpit170101012@iitg.ac.in>
2020-03-09 10:12:08 +0530

    Mappings changed to index based and table showing all the users formed

--- contracts/User.sol
+++ contracts/User.sol
@@ -1,5 +1,5 @@
 pragma solidity >=0.4.21 <0.7.0;
-
+// pragma experimental ABIEncoderV2;
 
 contract User {
 
@@ -8,6 +8,8 @@ contract User {
         uint8 Age;
         string Gender;
         address userid;
+        uint index;
+        // string address_string;
         // bytes32 ProfilePic; // Hash of profilepic
         address[] Posts;
         address[] Followers;    // Users who are following this user
@@ -16,41 +18,64 @@ contract User {
     }
 
     //Mappings are useful for O(1) searching 
-    mapping(address => userS) public usersM;
+    mapping(uint => userS) public usersM;
     mapping(address => mapping(address => int)) public followM; // Check if address1 follows address2
 
-    modifier checkuser(address id) {
+    // array containing addresses of all the users as you cannot iterate over the mapping
+    address[] usersA;
+    // total_users is the total count of users
+    uint public total_users;
+
+    function getusersAAt(uint idx) public returns(address){ 
+        return usersA[idx];
+    }
+
+
+    modifier checkuser(uint id) {
         userS memory u = usersM[id];
         require(u.Isuser, "User does not exist!");
         _;
     }
 
 
-
-    function getUserInfo() public view checkuser(msg.sender)
-    returns (string memory, uint8, string memory, address[] memory, address[] memory, address[] memory){
-        userS memory u = usersM[msg.sender];
-        return (u.Name, u.Age, u.Gender, u.Posts, u.Followers, u.Following);
+    function getUserInfo(uint idx) public returns (string memory, uint8, string memory, uint, address[] memory, address[] memory, address[] memory){
+        // address temp_address = usersA[idx];
+        userS memory u = usersM[idx];
+        return (u.Name, u.Age, u.Gender,u.index, u.Posts, u.Followers, u.Following);
     }
 
+    //TODO: check if user already exists
     function registerUser(string memory _name, uint8 _age, string memory _gender) public returns (bool){
-        userS memory u = usersM[msg.sender];
+        
+        // userS memory u = usersM[msg.sender];
         bytes memory testname = bytes(_name);
         bytes memory testgender = bytes(_gender);
         require(_age > 0 && _age < 200, "Age not correct");
         require(testname.length != 0, "Please enter name");
         require(testgender.length != 0, "Please enter gender");
-        require(!(u.userid>address(0x0)), "User already present");
-        usersM[msg.sender] = userS({Name:_name, Age:_age, Gender:_gender, userid:msg.sender,Posts:new address[](0), Followers:new address[](0), Following:new address[](0), Isuser:true});
+        // require(!userpresent(), "User already present");
+        // string memory address_string = AddresstoString(msg.sender);
+        usersM[total_users] = (userS({Name:_name, Age:_age, Gender:_gender,index:total_users, userid:msg.sender,Posts:new address[](0), Followers:new address[](0), Following:new address[](0), Isuser:true}));
+        usersA.push(msg.sender);
+        total_users++;
         return true;
     }
 
     function userpresent(string memory _address) public view returns (bool){
         address add = parseAddr(_address);
-        if (usersM[add].userid>address(0x0)) {
-            return true;
-        } else {
+        int idx = -1;
+        for(uint i = 0;i < total_users;i++)
+        {
+            if(usersA[i]==add)
+            {
+                idx = int(i);
+                break;
+            }
+        }
+        if (idx==-1) {
             return false;
+        } else {
+            return true;
         }
     }
 
--- src/js/app.js
+++ src/js/app.js
@@ -45,16 +45,36 @@ App = {
         // console.log(account);
         App.contracts.User.deployed().then(function(instance)
         {
+          App.userInstance = instance;
           // console.log(instance.userpresent(App.account));
           return instance.userpresent(App.account);
         }).then(function(x){
           // if user is already present in contract then directly show its page
+          // Fill up the table
           if(x)
           {
-            // console.log(App.account);
+            var Users = $('#Users');
+            Users.empty();
+            App.userInstance.total_users().then(function(total_users){
+              // console.log(total_users);
+              var value;
+              for(var i=0;i<total_users;i++)
+              {
+                    App.userInstance.getUserInfo.call(i).then(function(user){
+                      
+                      var name = user[0];
+                      var age = user[1];
+                      var gender = user[2];
+                      var value = user[3];
+                      var user_row = "<tr><th>" + name + "</th><td>" + age + "</td><td>" + gender + "</td><td>" + "<button class="+"btn btn-primary"+" type="+"button id='" + value +"' value='"+value +"' onclick = App.Follow('" + value + "')>Follow</button>" + "</td></tr>";
+                      Users.append(user_row);
+                    });
+
+              }
+            });
+            
             $("#accountAddress").html("Your Account: " + account);
             $('#register-btn').hide();
-            $('#login-btn').hide();
           }
           else
           {
@@ -66,38 +86,65 @@ App = {
 
 
   },
-  // If user is not present then clicking on login will take him to register page
-  // Login: function(){
-  //   web3.eth.getCoinbase(function(err, account){
-  //     if(err==null)
-  //     {
-  //       // console.log(account);
-  //       App.contracts.User.deployed().then(function(instance) {
-  //         var accounttemp = account;
-  //         // console.log(typeof accounttemp);
-  //         App.userInstance = instance;
-  //         // console.log(App.userInstance);
-  //         return App.userInstance.userpresent(account);
-  //       }).then(function(x) {
-  //         // console.log(account);
-  //         if (x) {
-  //           console.log(account);
-  //           // $('register-btn').hide();
-  //         }
-  //         else {
-  //           // console.log(account);
-  //           // $('login-btn').hide();
-  //           window.location = 'register.html';
-  //         }
-  //       });
-  //     }
-  //   });
-  // },
-  // If you are not already present in contract then clicking on register takes you on register page
+  
   Register: function() {
     
     window.location.replace('register.html');
   },
+
+
+
+    Signup_async: async function (params) {
+      let account1 = await web3.eth.getCoinbase(function(err, account){
+        if(err==null){App.account=account;} 
+      })
+
+        let userInstance1 = await App.contracts.User.deployed();
+        App.userInstance =  userInstance1;
+        var name = document.getElementById("name").value;
+        var age = document.getElementById("age").value;
+        var gender1 = document.getElementById("Male").checked;
+        var gender2 = document.getElementById("Female").checked;
+        var gender3 = document.getElementById("Transgender").checked;
+        var gender;
+        if(gender1==true) {gender="Male";}
+        else if(gender2==true){gender="Female";}
+        else if(gender3==true){gender="Transgender";}
+        else gender="";
+
+        let x = await App.userInstance.registerUser(name, age, gender);
+        if(x==1)
+        {
+          window.location.replace('index.html');
+        }
+
+    },
+
+    loadPage: function(){
+      window.location = 'index.html';
+    },
+
+    Follow: function(value){
+      // var button_value = $(this).val();
+      console.log(value);
+    }
+
+  //   toString:function (x){
+  //     var b = new bytes(20);
+  //     for (var i = 0; i < 20; i++)
+  //         b[i] = byte(uint8(uint(x) / (2**(8*(19 - i)))));
+  //     return string(b);
+  // }
+
+};
+
+$(function() {
+  $(window).load(function() {
+    App.init();
+  });
+});
+
+
 
   // Signup: function () {
       
@@ -131,53 +178,32 @@ App = {
   //     });      
   //   },
 
-    Signup_async: async function (params) {
-      let account1 = await web3.eth.getCoinbase(function(err, account){
-        if(err==null){App.account=account;} 
-      })
 
-      let userInstance1 = await App.contracts.User.deployed();
-      App.userInstance =  userInstance1;
-      // console.log(App.account);
-      // let y=await App.userInstance.userpresent(App.account);
-      // if(y)
-      // {
-      //   // console.log("abc");
-      //   window.location.replace('index.html');
-      // }
-      // else
-      // {
-        var name = document.getElementById("name").value;
-        var age = document.getElementById("age").value;
-        var gender1 = document.getElementById("Male").checked;
-        var gender2 = document.getElementById("Female").checked;
-        var gender3 = document.getElementById("Transgender").checked;
-        var gender;
-        if(gender1==true) {gender="Male";}
-        else if(gender2==true){gender="Female";}
-        else if(gender3==true){gender="Transgender";}
-        else gender="";
-
-        let x = await App.userInstance.registerUser(name, age, gender);
-        if(x==1)
-        {
-          window.location.replace('index.html');
-          // location.reload();
-        }
-      // }
-      
-
-    },
-
-    loadPage: function(){
-      window.location = 'index.html';
-      // location.reload();
-    }
-
-};
-
-$(function() {
-  $(window).load(function() {
-    App.init();
-  });
-});
+  // If user is not present then clicking on login will take him to register page
+  // Login: function(){
+  //   web3.eth.getCoinbase(function(err, account){
+  //     if(err==null)
+  //     {
+  //       // console.log(account);
+  //       App.contracts.User.deployed().then(function(instance) {
+  //         var accounttemp = account;
+  //         // console.log(typeof accounttemp);
+  //         App.userInstance = instance;
+  //         // console.log(App.userInstance);
+  //         return App.userInstance.userpresent(account);
+  //       }).then(function(x) {
+  //         // console.log(account);
+  //         if (x) {
+  //           console.log(account);
+  //           // $('register-btn').hide();
+  //         }
+  //         else {
+  //           // console.log(account);
+  //           // $('login-btn').hide();
+  //           window.location = 'register.html';
+  //         }
+  //       });
+  //     }
+  //   });
+  // },
+  // If you are not already present in contract then clicking on register takes you on register page
